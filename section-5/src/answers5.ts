2cc3edb7b0de88c86ce39d77c135d5784bc5a371
Emil Shafeev <amil0512@gmail.com>
2022-10-14 18:47:16 +0500

    - section 5 inited and answers almost done

---
+++ section-5/src/answers5.ts
@@ -0,0 +1,96 @@
+// 3
+
+interface Shoe {
+    purpose: string
+}
+
+class BalletFlat implements Shoe {
+    purpose = 'dancing'
+}
+
+class Boot implements Shoe {
+    purpose = 'woodyboody'
+}
+
+class Sneaker implements Shoe {
+    purpose = 'walking'
+}
+
+type Create = {
+    (type: 'ballet'): BalletFlat
+    (type: 'boot'): Boot
+}
+
+const create: Create = (type: 'ballet' | 'boot') => {
+    switch (type) {
+        case 'ballet':
+            return new BalletFlat()
+        case 'boot':
+            return new Boot()
+    }
+}
+
+const Shoe = {
+    // create(type: 'ballet' | 'boot' | 'sneaker'): Shoe {
+    //     switch (type) {
+    //         case 'ballet':
+    //             return new BalletFlat()
+    //         case 'boot':
+    //             return new Boot()
+    //         case 'sneaker':
+    //             return new Sneaker()
+    //     }
+    // },
+    // createTyped(
+    //     type: 'ballet' | 'boot' | 'sneaker'
+    // ): BalletFlat | Boot | Sneaker {
+    //     switch (type) {
+    //         case 'ballet':
+    //             return new BalletFlat()
+    //         case 'boot':
+    //             return new Boot()
+    //         case 'sneaker':
+    //             return new Sneaker()
+    //     }
+    // },
+    create(type: 'ballet' | 'boot' | 'sneaker') {
+        switch (type) {
+            case 'ballet':
+                return BalletFlat
+            case 'boot':
+                return new Boot()
+            case 'sneaker':
+                return new Sneaker()
+        }
+    },
+}
+
+const ballet = create('boot')
+
+// 4
+
+class AwsomeBuilder {
+    protected someData: string | null = null
+    protected someUrl: string | null = null
+
+    setData(data: string) {
+        this.someData = data
+        return new AwsomeBuilderWithData()
+    }
+}
+
+class AwsomeBuilderWithData extends AwsomeBuilder {
+    setUrl(url: string) {
+        this.someUrl = url
+        return new AwsomeBuilderWithUrlAndData()
+    }
+}
+class AwsomeBuilderWithUrlAndData extends AwsomeBuilderWithData {
+    send() {
+        if (!this.someUrl) throw new Error('Empty URL')
+        console.log(`URL: ${this.someUrl}  |  data: ${this.someData}`)
+    }
+}
+
+const builder = new AwsomeBuilder()
+builder.setData('data').setUrl('url/afe/afe/3').send()
